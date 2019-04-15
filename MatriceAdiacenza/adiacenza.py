10901bf7011bccacc14c508cdc4aa4498d61d43c
andrewcarpi <46714914+andrewcarpi@users.noreply.github.com>
2019-04-15 19:14:51 +0200

    Add files via upload

---
+++ MatriceAdiacenza/adiacenza.py
@@ -0,0 +1,10 @@
+m = [[0,0,0,1,0],[1,0,0,1,1],[0,0,0,1,0],[1,1,1,0,0],[0,1,0,0,0]]
+
+k = 0
+j = 0
+numNodi = 5
+
+for k in range(numNodi):
+    for j in range(numNodi):
+        if(m[k][j] == 1):
+print(str(k) + " è adiacente a " + str(j))
